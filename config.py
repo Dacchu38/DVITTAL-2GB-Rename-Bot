38e03daf05c588355103654caf0913e10d72db6e
Preethudg <sanjanasanjanadg59@gmail.com>
2025-03-17 09:22:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -47,7 +47,7 @@ class Config(object):
  
     # other configs
     RKN_PIC = os.environ.get("RKN_PIC", "https://telegra.ph/file/b746aadfe59959eb76f59.jpg")
-    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6705898491').split()]
+    ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '2089142494').split()]
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002334874259"))
 
     # free upload limit 
